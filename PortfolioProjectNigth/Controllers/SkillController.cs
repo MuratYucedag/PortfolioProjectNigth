280faaedf8cb4334e0dd132558c708c83f1ddab7
MuratYucedag <yucedagmurat23@gmail.com>
2024-09-19 21:44:12 +0300

    ...

--- PortfolioProjectNigth/Controllers/SkillController.cs
+++ PortfolioProjectNigth/Controllers/SkillController.cs
@@ -15,5 +15,33 @@ namespace PortfolioProjectNigth.Controllers
             var values = context.Skill.ToList();
             return View(values);
         }
+
+        [HttpGet]
+        public ActionResult CreateSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateSkill(Skill skill)
+        {
+            context.Skill.Add(skill);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+        public ActionResult DeleteSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            context.Skill.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("SkillList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateSkill(int id)
+        {
+            var value = context.Skill.Find(id);
+            return View(value);
+        }
     }
 }
