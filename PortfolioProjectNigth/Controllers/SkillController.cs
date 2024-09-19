458197416e4858340aa8a0d7e89997f28446f7ac
MuratYucedag <yucedagmurat23@gmail.com>
2024-09-19 20:44:44 +0300

    ...

---
+++ PortfolioProjectNigth/Controllers/SkillController.cs
@@ -0,0 +1,19 @@
+﻿using PortfolioProjectNigth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortfolioProjectNigth.Controllers
+{
+    public class SkillController : Controller
+    {
+        DbMyPortfolioNightEntities context = new DbMyPortfolioNightEntities();
+        public ActionResult SkillList()
+        {
+            var values = context.Skill.ToList();
+            return View(values);
+        }
+    }
+}
